548e5fb3acaf4b11e506e1979d2662a41566d683
hamza <hmzaitahmad@gmail.com>
2024-06-06 11:55:22 +0100

    add solu

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Home from '../components/Home/HomePage';
 import Header from "../components/Header/Header";
-import Solutions from "../components/Solutions/Solutions";
 import About from "../components/About/About";
+import Solutions from "../components/Solutions/Solutions";
 
 const home = () => {
     return (
