81c849624324f9941fde65924e86b089aba6cd86
AYMAN-AZ-HUB <a36282303@gmail.com>
2024-06-07 17:57:27 +0100

    cards component

--- src/pages/Formation.jsx
+++ src/pages/Formation.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Formation from "../components/services/formation/formation";
 import Folder from "../components/services/formation/main";
+import Media from "../components/services/formation/cards-text";
 
 const formation = () => {
   return (
     <>
       <Formation />
       <Folder />
+      <Media />
     </>
   );
 };
