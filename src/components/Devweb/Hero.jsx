93f3ebee963b2083e442b9b6a280e2ec34278a15
IAMTHEWONDERER <theoblivionitself@gmail.com>
2024-06-07 14:48:12 +0100

    added extra changes for the hero section

--- src/components/Devweb/Hero.jsx
+++ src/components/Devweb/Hero.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import background from '../../images/backgroundwebdev.png'; // Adjust the path as necessary
-import personPlaceholder from '../../images/personwebdev.png'; // Adjust the path as necessary
+    import React from 'react';
+    import background from '../../images/backgroundwebdev.png'; // Adjust the path as necessary
+    import personPlaceholder from '../../images/personwebdev1.png'; // Adjust the path as necessary
+    import './button.css'
+    const Hero = () => {
+    return (
+        <div
+        className="flex flex-col lg:flex-row items-center justify-between p-8 bg-cover bg-center h-screen text-white font-poppins"
+        style={{ backgroundImage: `url(${background})` }}
+      >
+   <div className="max-w-xl mb-8 lg:mb-0 text-center lg:text-left mx-auto" style={{ marginTop: '17vh' }}>
+  <h1 className="text-4xl lg:text-8xl font-bold mb-4">Développement Web et Mobile</h1>
+  <p className="text-lg lg:text-xl mb-8">
+    Nous vous accompagnons dans le conseil, la mise en place, le suivi et l’optimisation
+    des campagnes de référencement organique et payant pour réussir vos projets.
+  </p>
+  <button className="custom-button">Play a demo <span className="circle">&gt;</span></button>
+</div>
 
-const Hero = () => {
-  return (
-    <div
-      className="flex items-center justify-between p-8 bg-cover bg-center h-screen text-white font-poppins"
-      style={{ backgroundImage: `url(${background})` }}
-    >
-      <div className="max-w-xl">
-        <h1 className="text-8xl font-bold mb-4">Développement Web et Mobile</h1>
-        <p className="text-xl mb-8">
-          Nous vous accompagnons dans le conseil, la mise en place, le suivi et l’optimisation
-          des campagnes de référencement organique et payant pour réussir vos projets.
-        </p>
-        <button className="bg-white text-black py-3 px-6 rounded text-lg">Play a demo</button>
-      </div>
-      <div className="hidden lg:block lg:flex-shrink-0 lg:ml-32 lg:h-full mt-16">
-        <img src={personPlaceholder} alt="Person holding a laptop" className="h-full object-cover" />
-      </div>
-    </div>
-  );
-};
 
-export default Hero;
+
+        <div className="hidden lg:block lg:flex-shrink-0 lg:h-full mt-16">
+          <img src={personPlaceholder} alt="Person holding a laptop" className="h-full object-cover" />
+        </div>
+      </div>
+      
+      
+    );
+    };
+
+    export default Hero;
