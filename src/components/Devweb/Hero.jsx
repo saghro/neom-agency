f2bc1654059d7c9d2a69204268af25fb6b9e857d
IAMTHEWONDERER <theoblivionitself@gmail.com>
2024-06-07 11:30:18 +0100

    added font poppins and fixed some hero section code

--- src/components/Devweb/Hero.jsx
+++ src/components/Devweb/Hero.jsx
@@ -4,17 +4,20 @@ import personPlaceholder from '../../images/personwebdev.png'; // Adjust the pat
 
 const Hero = () => {
   return (
-    <div className="flex items-center justify-between p-8 bg-cover bg-center h-screen text-white" style={{ backgroundImage: `url(${background})` }}>
+    <div
+      className="flex items-center justify-between p-8 bg-cover bg-center h-screen text-white font-poppins"
+      style={{ backgroundImage: `url(${background})` }}
+    >
       <div className="max-w-xl">
-        <h1 className="text-5xl font-bold mb-4">Développement Web et Mobile</h1>
+        <h1 className="text-8xl font-bold mb-4">Développement Web et Mobile</h1>
         <p className="text-xl mb-8">
           Nous vous accompagnons dans le conseil, la mise en place, le suivi et l’optimisation
           des campagnes de référencement organique et payant pour réussir vos projets.
         </p>
         <button className="bg-white text-black py-3 px-6 rounded text-lg">Play a demo</button>
       </div>
-      <div className="flex-shrink-0">
-        <img src={personPlaceholder} alt="Person holding a laptop" className="max-h-96" />
+      <div className="hidden lg:block lg:flex-shrink-0 lg:ml-32 lg:h-full mt-16">
+        <img src={personPlaceholder} alt="Person holding a laptop" className="h-full object-cover" />
       </div>
     </div>
   );
