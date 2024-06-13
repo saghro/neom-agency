4bb749a56a88adc6192667da43c2ce5d605db4f2
fati-aafak <fatimaahamam123@gmail.com>
2024-06-13 16:01:07 +0200

    change img seo

--- src/components/Services/Seo/Seo.jsx
+++ src/components/Services/Seo/Seo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-  import personPlaceholder from '../../../assests/seo.png'; 
+  import personPlaceholder from '../../../assests/woman-posing.png'; 
   import './button.css'
     const Hero = () => {
     return (
