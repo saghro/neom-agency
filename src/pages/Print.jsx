baceee4ec3fe6ca866abc14474a0f02d7a7385c5
AYMAN-AZ-HUB <a36282303@gmail.com>
2024-06-20 09:49:22 +0100

    updates

--- src/pages/Print.jsx
+++ src/pages/Print.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
-import MiddleSection from '../components/Services/print/MiddleSection'
-import WebCards from '../components/Services/print/WebCards'
-import PageCards from '../components/Services/print/LinkCard'
-import Hero from '../components/Services/print/Hero'
+import MiddleSection from '../components/services/print/MiddleSection'
+import WebCards from '../components/services/print/WebCards'
+import PageCards from '../components/services/print/LinkCard'
+import Hero from '../components/services/print/Hero'
 const Webdev = () => {
     return (
         <>
