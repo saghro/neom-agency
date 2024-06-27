c9c46cbd29ba49a38094a797714f213d2781dd36
AYMAN-AZ-HUB <a36282303@gmail.com>
2024-06-27 10:08:45 +0100

    updates realisation page

--- src/components/Services/Social/DevCases.jsx
+++ src/components/Services/Social/DevCases.jsx
@@ -4,14 +4,15 @@ import Image1 from '../../../assests/identite1.png';
 import Image2 from '../../../assests/identi2.png';
 import Image3 from '../../../assests/identi3.png';
 import Image4 from '../../../assests/identi4.png';
+import { Link } from 'react-router-dom';
 
-
+ 
 const DevCases = () => {
   const colors = ['#A8D9E0', '#C7A2CB', '#6A71B3', '#B3A2D9'];
  
   return (
     <div>
-      <h1 className="text-3xl font-bold font-Rammetto-One bg-gradient-to-r from-[#C7A2CB] via-[#A8D9E0] to-[#6A71B3] text-transparent bg-clip-text text-center mt-6 mb-4 lg:mb-0">Realisation</h1>
+      <h1 className="text-3xl font-bold font-Rammetto-One bg-gradient-to-r from-[#C7A2CB] via-[#A8D9E0] to-[#6A71B3] text-transparent bg-clip-text mt-8 text-center">Etude des cas</h1>
       <div className="grid grid-cols-1 md:grid-cols-2">
         <Cases
           imageSrc={Image1}
@@ -41,13 +42,15 @@ const DevCases = () => {
           backgroundColor={colors[3]}
         /> 
       </div>
-      <div className="flex justify-center">
-  <div className="h-14 lg:w-56 rounded-3xl bg-gradient-to-r from-[#C7A2CB] via-[#A8D9E0] to-[#6A71B3] p-0.5 flex items-center justify-center">
-    <button className="text-xl text-[#6A71B3]">Voir plus</button>
-  </div>
-</div>
-
-
+      <div className="border-b-2 border-gray-300 my-4 mx-auto w-1/2"></div>
+        <div className="flex justify-center">
+          <div className="h-14 lg:w-56 rounded-3xl p-0.5 flex items-center justify-center" style={{ border: '2px solid', borderImage: 'linear-gradient(to right, #6A71B3, #A8D9E0, #C7A2CB) 1' }}>
+            <Link to="/realisation">
+              <button className="text-xl transition-colors duration-300 hover:bg-gray-200" style={{ background: 'transparent', color: '#00008B' }}>Voir plus</button>
+            </Link>
+          </div>
+        </div>
+      
     </div>
   );
 }
