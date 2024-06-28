22a298da84a30a416f8e44492aada871a3da9ff3
AYOUB <ayoubsaghro27@gmail.com>
2024-06-28 12:31:01 +0100

    ne

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -25,11 +25,7 @@ const Router = () => {
                     <Route path="/" element={<HomePage />} />
                     <Route path="/web" element={<Webdev />} />
                     <Route path="/realisation" element={<Realisation />} />
-                    <Route path="/communication" element={<Print />} />
-                    <Route path="/identité-de-marque" element={<Service />} />
-                    <Route path="/markting" element={<Seo />} />
-                    <Route path="/solutions" element={<Solutions />} />
-                    <Route path="/contact" element={<ContactPage />} />
+                   
                 </Routes>
             </ScrollToTopWrapper>
         </BrowserRouter>
