19c4d42579b3f8d65e884addc9a4926426086d3a
fati-aafak <fatimaahamam123@gmail.com>
2024-06-28 10:48:10 +0100

    updates

--- src/components/cases.jsx
+++ src/components/cases.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import './cases.css';
- 
+import './Cases.css'; // Import custom CSS file
+
 const Cases = ({ imageSrc, title, text }) => {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
- 
+  const [mousePosition, setMousePosition] = useState({ x: 128, y: 128 });
+
   const handleMouseMove = (e) => {
     const rect = e.currentTarget.getBoundingClientRect();
     setMousePosition({
@@ -12,22 +12,22 @@ const Cases = ({ imageSrc, title, text }) => {
       y: e.clientY - rect.top,
     });
   };
- 
+
   const calculateTilt = () => {
     const centerX = 128; // Half of the width (256 / 2)
     const centerY = 128; // Half of the height (256 / 2)
     const deltaX = mousePosition.x - centerX;
     const deltaY = mousePosition.y - centerY;
     const maxTilt = 25; // Increased maximum tilt angle in degrees
- 
+
     const tiltX = (deltaY / centerY) * maxTilt;
     const tiltY = -(deltaX / centerX) * maxTilt;
- 
+
     return { tiltX, tiltY };
   };
- 
+
   const { tiltX, tiltY } = calculateTilt();
- 
+
   return (
     <div className="flex flex-col md:flex-row justify-center items-center p-4 ">
       <div
@@ -51,7 +51,7 @@ const Cases = ({ imageSrc, title, text }) => {
               animation: AnimationName 6s ease infinite;
               transition: background 0.4s ease;
             }
- 
+
             @keyframes AnimationName {
               0%{background-position:0% 30%}
               50%{background-position:100% 71%}
@@ -60,12 +60,12 @@ const Cases = ({ imageSrc, title, text }) => {
           `}
         </style>
       </div>
-      <div className="flex flex-col justify-start p-4 bg-white shadow-lg rounded-lg w-64 h-64 text-center mt-4 mb-4">
+      <div className="flex flex-col justify-start p-4 bg-[#fdfcfe] shadow-lg rounded-lg w-64 h-64 text-center mt-4 mb-4">
         <h3 className="text-lg font-bold">{title}</h3>
         <p className="mt-2 text-sm">{text}</p>
       </div>
     </div>
   );
 };
- 
+
 export default Cases;
